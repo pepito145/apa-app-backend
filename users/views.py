a996eb6e8ece0edf716694ff44f72b908a2e7e62
HUANG Zhaoyuan <104513555+ja99ovo@users.noreply.github.com>
2025-01-13 19:13:49 +0100

    Update views.py

--- users/views.py
+++ users/views.py
@@ -129,14 +129,13 @@ class Get_code(APIView):
             data = response.json()
             #user = UserLogin.objects.get(clientid=response.state)
             # 更新用户的 token
-            user.access_token = data.access_token
+            user.access_token = data['body']['access_token']
             user.save()
-            return {'data': data}
         except requests.exceptions.RequestException as e:
             return {
                 'error': 'An error occurred while sending the POST request',
                 'details': str(e),
-                'data': data,
+                'data': data['body']['access_token'],
             }
             
             
