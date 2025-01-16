5a029311fc35729fcde0a2f6ada386537def132f
HUANG Zhaoyuan <104513555+ja99ovo@users.noreply.github.com>
2025-01-16 06:05:28 +0100

    Update views.py

--- users/views.py
+++ users/views.py
@@ -192,8 +192,8 @@ class Get_activity(APIView):
                     date=date,
                 )
             except json.JSONDecodeError:
-                return JsonResponse(status=200)
+                return JsonResponse({"status": "success"}, status=200)
                 #return JsonResponse({'status': 'error', 'message': 'Invalid JSON'}, status=400)
         else:
-            return JsonResponse(status=200)
+            return JsonResponse({"status": "success"}, status=200)
             #return JsonResponse({'status': 'error', 'message': 'Invalid method'}, status=405)
