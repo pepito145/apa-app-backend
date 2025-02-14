b2d1795fb5f2f496a2a22b19d73481f4f550672f
HUANG Zhaoyuan <104513555+ja99ovo@users.noreply.github.com>
2025-02-14 05:56:55 +0100

    Update models.py

--- users/models.py
+++ users/models.py
@@ -41,7 +41,7 @@ class Activity(models.Model):
     end_date = models.DateTimeField(null=True)
     date = models.DateTimeField(null=True)
     activity = models.CharField(max_length=20, blank=True, null=True)
-    
+    seance_id = models.IntegerField(blank=True, null=True)
     class Meta:
         db_table = 'activity'
     
