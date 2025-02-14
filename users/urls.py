73db83dd0955a4ab6cbe30b8f737fba52379d4eb
HUANG Zhaoyuan <104513555+ja99ovo@users.noreply.github.com>
2025-02-14 05:07:43 +0100

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import RegisterView, LoginView, Client_id, Get_code, Get_activity
+from .views import RegisterView, LoginView, Client_id, Get_code, Get_activity, get_seance
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -7,5 +7,6 @@ urlpatterns = [
     path('client_id/', Client_id.as_view(), name='client_id'),
     path('get_code/', Get_code.as_view(), name='get_code'),
     path('get_activity/', Get_activity.as_view(), name='get_activity'),
+    path('get_seance/', get_seance.as_view(), name='get_seance'),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
