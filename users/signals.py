05b3b7b24ca5f4f091e927375d59ec0ea111b00f
HUANG Zhaoyuan <104513555+ja99ovo@users.noreply.github.com>
2025-04-03 13:50:27 +0200

    Update signals.py

--- users/signals.py
+++
@@ -1,8 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .models import UserLogin, UsersInfos
-
-@receiver(post_save, sender=UserLogin)
-def create_user_infos(sender, instance, created, **kwargs):
-    if created:
-        UsersInfos.objects.create(user=instance)
